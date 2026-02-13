7e8f88b9057f82956963d7138889d94dca4540eb
Yujonpradhananga <yujonpradhan123@gmail.com>
2026-02-13 16:55:06 +0545

    chapter 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,29 +1,15 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=10);
-    println!("the secrete number is {secret_number}");
-    loop {
-        println!("Enter your number!");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("the  number you guessed is {}", guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("too smol"),
-            Ordering::Greater => println!("greater niga"),
-            Ordering::Equal => {
-                println!("You win! ");
-                break;
-            }
+    let s2 = "nigga nigga";
+    let word = first_word(&s2);
+    println!("{}", word)
+}
+
+fn first_word(word: &str) -> &str {
+    let bytes = word.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &word[0..i];
         }
     }
+    &word[..]
 }
