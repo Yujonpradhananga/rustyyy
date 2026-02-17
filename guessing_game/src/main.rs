7d0faff7c5988d2532ffad0dc20d179ef4870a77
Yujonpradhananga <yujonpradhan123@gmail.com>
2026-02-17 18:13:43 +0545

    chapter 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,24 @@
-fn main() {
-    let s2 = "nigga nigga";
-    let word = first_word(&s2);
-    println!("{}", word)
-}
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-fn first_word(word: &str) -> &str {
-    let bytes = word.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &word[0..i];
+fn main() {
+    println!("Enter a number:");
+    let sec_num = rand::thread_rng().gen_range(1..=50);
+    let mut guess = String::new();
+    loop {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to load number");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!!!");
+        println!("You guessed {guess}");
+        match guess.cmp(&sec_num) {
+            Ordering::Less => println!("number guessed is too low!"),
+            Ordering::Greater => println!("number guessed is too big!"),
+            Ordering::Equal => {
+                println!("You guessed the correct number!!!");
+                break;
+            }
         }
     }
-    &word[..]
 }
